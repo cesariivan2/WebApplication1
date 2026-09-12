aee116115c83da9a53907634aba45c8ffb8360ea
cesariivan2 <cesariivan2@gmail.com>
2026-09-12 08:25:57 -0700

    Commit: Agrega estructura de torneo de videojuegos

---
+++ WebApplication1/Models/ParticipanteTorneo.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class ParticipanteTorneo
+    {
+        public int Id { get; set; }
+
+        public int TorneoVideojuegoId { get; set; }
+
+        public int AlumnoId { get; set; }
+
+        public DateTime FechaInscripcion { get; set; } = DateTime.Now;
+
+        [Required]
+        [StringLength(30)]
+        public string Estado { get; set; } = "Inscrito";
+
+        public TorneoVideojuego? TorneoVideojuego { get; set; }
+
+        public Alumno? Alumno { get; set; }
+    }
+}
