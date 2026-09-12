7f2f90d1b1fc7c0a594971dbb6e9459cb7c72c96
zanchexchumimi-ai <zanchexchumimi@gmail.com>
2026-09-12 10:05:55 -0600

    gzd

---
+++ WebApplication1/Views/Alumnos/Migafete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Views.Alumnos
+{
+    public class MigafeteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
