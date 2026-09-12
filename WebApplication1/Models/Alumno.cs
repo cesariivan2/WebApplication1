f00e753da6170336ad7ba57cdfc01be3622e0d07
cesariivan2 <cesariivan2@gmail.com>
2026-09-12 01:42:37 -0700

    obligamos a LA HORA DEL REGISTRO QUE NO HAYA DATOS NULOS EN CORREO Y NOMBRE

--- WebApplication1/Models/Alumno.cs
+++ WebApplication1/Models/Alumno.cs
@@ -1,13 +1,16 @@
-﻿namespace WebApplication1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
 {
     public class Alumno
     {
         public int Id { get; set; }
-
+        [Required]
         public string Nombre { get; set; } = string.Empty;
 
         public string Matricula { get; set; } = string.Empty;
-
+        [Required]
+        [EmailAddress]
         public string Correo { get; set; } = string.Empty;
 
         public string Turno { get; set; } = string.Empty;
