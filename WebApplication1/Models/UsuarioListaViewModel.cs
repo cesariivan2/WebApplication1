ba314a9238185f84dc8b824b6d1f6090c7a2880c
cesariivan2 <cesariivan2@gmail.com>
2026-09-12 12:45:47 -0700

    ddw

---
+++ WebApplication1/Models/UsuarioListaViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Models
+{
+    public class UsuarioListaViewModel
+    {
+        public string Id { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Roles { get; set; } = string.Empty;
+    }
+}
