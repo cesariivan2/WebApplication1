f8c3ba96a0c0d0ea50620474c0b19581996be1cc
ESCUELA <ESCUELA@DESKTOP-T11PR70>
2026-09-11 18:01:24 -0700

    Agregar archivos de proyecto.

---
+++ WebApplication1/Migrations/20260912005327_CrearBaseInicial.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class CrearBaseInicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Alumnos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Matricula = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Correo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Turno = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alumnos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Alumnos");
+        }
+    }
+}
---
+++ WebApplication1/Models/Alumno.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication1.Models
+{
+    public class Alumno
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; } = string.Empty;
+
+        public string Matricula { get; set; } = string.Empty;
+
+        public string Correo { get; set; } = string.Empty;
+
+        public string Turno { get; set; } = string.Empty;
+    }
+}
